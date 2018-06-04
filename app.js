78e423f6828e8650cd01de9a04e9340e398126b2
Kira Yang <wenqin.z.yang@gmail.com>
2018-06-04 15:35:59 -0400

    allow cors
    test commit to allow cross domain requests

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const config = require('./config');
 const setupController = require('./controllers/setupController');
 const apiController = require('./controllers/apiController');
 
 const port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/public'));
+//app.use('/assets', express.static(__dirname + '/public'));
+app.use(cors({ credentials: true, origin: true }));
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
